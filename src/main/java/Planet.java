2f5eded5c288739f9798b14fe141e2442bf69eba
colette <c.dufficy@gmail.com>
2022-09-05 14:28:29 +0100

    initial commit

---
+++ src/main/java/Planet.java
@@ -0,0 +1,45 @@
+public class Planet {
+    private String name;
+
+    public Planet (String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+//    public String mars() {
+//        return this.name;
+//    }
+
+//    public String getSize() {
+//        return this.size;
+//    }
+
+}
+
+
+//class Planet:
+//
+//        def __init__(self, name, size):
+//        self.name = name
+//        self.size = size
+//
+//        def get_name(self):
+//        return self.name
+//
+//
+//        def get_size(self):
+//        return self.size
+//
+//        def explode(self):
+//        print(f"Boom! {self.name} has exploded.")
+//
+//
+//        def __main__():
+//        mars = Planet("Mars", 908973)
+//        print(f"{mars.get_name()} is {mars.get_size()}")
+//        mars.explode()
+//
+//        __main__()
---
+++ src/main/java/Runner.java
@@ -0,0 +1,9 @@
+public class Runner {
+    public static void main(String[] args) {
+        Planet planet = new Planet();
+        System.out.println(planet.getName());
+    }
+
+}
+
+
